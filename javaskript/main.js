372041f96d3d71f4fc485ef7fe3088bb6ae0d821
AkylbekBosyn <96645960+AkylbekBosyn@users.noreply.github.com>
2022-02-02 18:52:26 +0600

    Add files via upload

---
+++ javaskript/main.js
@@ -0,0 +1,13 @@
+function myfun() 
+{ 
+    var a = document.getElementById("number").value; 
+    var b = document.getElementById("number").value; 
+    document.getElementById("result").innerHTML = (a * b); 
+} 
+
+function myfun2() 
+{ 
+    var a = document.getElementById("number").value; 
+    var b = document.getElementById("number").value; 
+    document.getElementById("result").innerHTML =( a / b);  
+}
